a43d94e8d13b2190943033f7a8bc0dd98bff83fa
JacquelineK01 <61940347+JacquelineK01@users.noreply.github.com>
2020-04-22 12:20:01 +0300

    Update FileRepository.h

--- Lab08/Lab08/FileRepository.h
+++ Lab08/Lab08/FileRepository.h
@@ -11,17 +11,17 @@ private:
 	std::vector<CalatorieAutobuz> calatoriiAutobuz;
 	std::vector<CalatorieAvion> calatoriiAvion;
 	bool textOrCSV;
-	char* airplaneTripsFileName;
-	char* busTripsFileName;
-	char* writeFileName;
+	char airplaneTripsFileName[100];
+	char busTripsFileName[100];
+	char writeFileName[100];
 public:
-	FileRepository(const char* tripsFileName, const char* busTripsFileName, const char* writeFileName, bool textOrCSV = 0);
+	FileRepository(const char* airplaneTripsFileName, const char* busTripsFileName, const char* writeFileName, bool textOrCSV = 0);
 	FileRepository();
 	~FileRepository();
 	void loadBusTripsFromFile();
 	void loadAirplaneTripsFromFile();
-	void writeTripsToTxtFile();
-	void writeTripsToCSVFile();
+	void writeTripsToTxtFile(std::vector<CalatorieAutobuz> calatoriiAutobuz, std::vector<CalatorieAvion> calatoriiAvion);
+	void writeTripsToCSVFile(std::vector<CalatorieAutobuz> calatoriiAutobuz, std::vector<CalatorieAvion> calatoriiAvion);
 	void changeRepoType(bool textOrCSV);
 	std::vector<CalatorieAutobuz> getAllBusTrips();
 	std::vector<CalatorieAvion> getAllAirplaneTrips();

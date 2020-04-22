6590a419aa1bcb9692e3f8bab6c170eb6816e731
JacquelineK01 <61940347+JacquelineK01@users.noreply.github.com>
2020-04-22 12:26:37 +0300

    Add files via upload

---
+++ Lab08/Lab08/main.cpp
@@ -0,0 +1,13 @@
+#include "UI.h"
+int main() {
+	char airplane[100],bus[100], write[100];
+	strcpy_s(airplane,sizeof("airplane.txt"), "airplane.txt");
+	strcpy_s(bus, sizeof("bus.txt"), "bus.txt");
+	strcpy_s(write, sizeof("test.txt"), "test.txt");
+	FileRepository repo{ airplane, bus, write };
+	LogInService logInService{ "users.txt" };
+	Service service{ repo };
+	UI ui{ service, logInService };
+	ui.show_menu_1();
+	return 0;
+}

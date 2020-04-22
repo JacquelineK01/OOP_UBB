73149a3df3a7b47874b678cb74d3f14e789b8584
JacquelineK01 <61940347+JacquelineK01@users.noreply.github.com>
2020-04-22 12:28:25 +0300

    Add files via upload

---
+++ Lab08/Lab08/Tests.cpp
@@ -0,0 +1,20 @@
+#include "Tests.h"
+#include <assert.h>
+
+/*
+void testFileRepo() {
+	FileRepository repo{ "airplaneTest.txt", "busTest.txt", "tests.txt" };
+	repo.loadAirplaneTripsFromFile();
+	repo.loadBusTripsFromFile();
+	std::vector<CalatorieAutobuz>bus = repo.getAllBusTrips();
+	assert(bus.size() == 1);
+	repo.writeTripsToTxtFile();
+	throw{
+		repo.rezervaLocuri(...);
+		assert(false);
+	}
+	catch{
+		assert(true);
+	}
+}
+*/

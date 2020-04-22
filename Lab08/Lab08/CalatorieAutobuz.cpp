3160ec97a7aaa43658ed5a229a99fae22a041f67
JacquelineK01 <61940347+JacquelineK01@users.noreply.github.com>
2020-04-22 12:17:52 +0300

    Update CalatorieAutobuz.cpp

--- Lab08/Lab08/CalatorieAutobuz.cpp
+++ Lab08/Lab08/CalatorieAutobuz.cpp
@@ -12,13 +12,10 @@ CalatorieAutobuz::CalatorieAutobuz(int cod, const char* localitate_plecare, cons
 CalatorieAutobuz::CalatorieAutobuz(const CalatorieAutobuz& b){
 	this->cod = b.cod;
 
-	this->localitate_plecare = new char[strlen(b.localitate_plecare) + 1];
 	strcpy_s(this->localitate_plecare, strlen(b.localitate_plecare) + 1, b.localitate_plecare);
 
-	this->localitate_destinatie = new char[strlen(b.localitate_destinatie) + 1];
 	strcpy_s(this->localitate_destinatie, strlen(b.localitate_destinatie) + 1, b.localitate_destinatie);
 
-	this->data_plecarii = new char[strlen(b.data_plecarii) + 1];
 	strcpy_s(this->data_plecarii, strlen(b.data_plecarii) + 1, b.data_plecarii);
 
 	this->nr_zile_durata = b.nr_zile_durata;
@@ -41,24 +38,6 @@ void CalatorieAutobuz::setNrZileDurata(int x)
 CalatorieAutobuz::~CalatorieAutobuz()
 {
 	this->cod = 0;
-
-	if (this->localitate_plecare)
-	{
-		delete[] this->localitate_plecare;
-		this->localitate_plecare == NULL;
-	}
-
-	if (this->localitate_destinatie)
-	{
-		delete[] this->localitate_destinatie;
-		this->localitate_destinatie == NULL;
-	}
-
-	if (this->data_plecarii)
-	{
-		delete[] this->data_plecarii;
-		this->data_plecarii == NULL;
-	}
 
 	this->nr_zile_durata = 0;
 

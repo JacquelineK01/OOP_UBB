a82f03bf6145146da4b1371f7dce44f2fb32b3bb
JacquelineK01 <61940347+JacquelineK01@users.noreply.github.com>
2020-04-22 12:16:31 +0300

    Update Calatorie.cpp

--- Lab08/Lab08/Calatorie.cpp
+++ Lab08/Lab08/Calatorie.cpp
@@ -4,9 +4,9 @@
 Calatorie::Calatorie()
 {
 	this->cod = 0;
-	this->localitate_plecare = NULL;
-	this->localitate_destinatie = NULL;
-	this->data_plecarii = NULL;
+	strcpy(this->localitate_plecare," ");
+	strcpy(this->localitate_destinatie, " ");
+	strcpy(this->data_plecarii, " ");
 	this->nr_loc_totale = 0;
 	this->nr_loc_rezervate = 0;
 }
@@ -19,13 +19,10 @@ Calatorie::Calatorie(int cod,
 	int nr_loc_rezervate){
 	this->cod = cod;
 
-	this->localitate_plecare = new char[strlen(localitate_plecare) + 1];
 	strcpy_s(this->localitate_plecare, strlen(localitate_plecare) + 1, localitate_plecare);
 
-	this->localitate_destinatie = new char[strlen(localitate_destinatie) + 1];
 	strcpy_s(this->localitate_destinatie, strlen(localitate_destinatie) + 1, localitate_destinatie);
 
-	this->data_plecarii = new char[strlen(data_plecarii) + 1];
 	strcpy_s(this->data_plecarii, strlen(data_plecarii) + 1, data_plecarii);
 
 	this->nr_loc_totale = nr_loc_totale;
@@ -37,13 +34,10 @@ Calatorie::Calatorie(const Calatorie& a)
 {
 	this->cod = a.cod;
 
-	this->localitate_plecare = new char[strlen(a.localitate_plecare) + 1];
 	strcpy_s(this->localitate_plecare, strlen(a.localitate_plecare) + 1, a.localitate_plecare);
 
-	this->localitate_destinatie = new char[strlen(a.localitate_destinatie) + 1];
 	strcpy_s(this->localitate_destinatie, strlen(a.localitate_destinatie) + 1, a.localitate_destinatie);
 
-	this->data_plecarii = new char[strlen(a.data_plecarii) + 1];
 	strcpy_s(this->data_plecarii, strlen(a.data_plecarii) + 1, a.data_plecarii);
 
 	this->nr_loc_totale = a.nr_loc_totale;
@@ -55,23 +49,6 @@ Calatorie::~Calatorie()
 {
 	this->cod = 0;
 
-	if (this->localitate_plecare)
-	{
-		delete[] this->localitate_plecare;
-		this->localitate_plecare == NULL;
-	}
-
-	if (this->localitate_destinatie)
-	{
-		delete[] this->localitate_destinatie;
-		this->localitate_destinatie == NULL;
-	}
-
-	if (this->data_plecarii)
-	{
-		delete[] this->data_plecarii;
-		this->data_plecarii == NULL;
-	}
 	this->nr_loc_totale = 0;
 
 	this->nr_loc_rezervate = 0;
@@ -113,34 +90,19 @@ int Calatorie::getNrLocRezervate()
 
 void Calatorie::setLocalitatePlecare(const char* localitate_plecare)
 {
-	if (this->localitate_plecare != NULL)
-	{
-		delete[] this->localitate_plecare;
-		this->localitate_plecare = NULL;
-	}
-	this->localitate_plecare = new char[strlen(localitate_plecare) + 1];
+
 	strcpy_s(this->localitate_plecare, strlen(localitate_plecare) + 1, localitate_plecare);
 }
 
 void Calatorie::setLocalitateDestinatie(const char* localitate_destinatie)
 {
-	if (this->localitate_destinatie != NULL)
-	{
-		delete[] this->localitate_destinatie;
-		this->localitate_destinatie = NULL;
-	}
-	this->localitate_destinatie = new char[strlen(localitate_destinatie) + 1];
+
 	strcpy_s(this->localitate_destinatie, strlen(localitate_destinatie) + 1, localitate_destinatie);
 }
 
 void Calatorie::setDataPlecarii(const char* data_plecarii)
 {
-	if (this->data_plecarii != NULL)
-	{
-		delete[] this->data_plecarii;
-		this->data_plecarii = NULL;
-	}
-	this->data_plecarii = new char[strlen(data_plecarii) + 1];
+
 	strcpy_s(this->data_plecarii, strlen(data_plecarii) + 1, data_plecarii);
 }
 
@@ -161,29 +123,9 @@ void Calatorie::setNrLocRezervate(int nr_loc_rezervate)
 Calatorie& Calatorie::operator=(const Calatorie& a)
 {
 	this->cod = a.cod;
-
-	if (this->localitate_plecare)
-	{
-		delete[] this->localitate_plecare;
-		this->localitate_plecare == NULL;
-	}
-	this->localitate_plecare = new char[strlen(a.localitate_plecare) + 1];
 	strcpy_s(this->localitate_plecare, strlen(a.localitate_plecare) + 1, a.localitate_plecare);
-
-	if (this->localitate_destinatie)
-	{
-		delete[] this->localitate_destinatie;
-		this->localitate_destinatie == NULL;
-	}
-	this->localitate_destinatie = new char[strlen(a.localitate_destinatie) + 1];
 	strcpy_s(this->localitate_destinatie, strlen(a.localitate_destinatie) + 1, a.localitate_destinatie);
 
-	if (this->data_plecarii)
-	{
-		delete[] this->data_plecarii;
-		this->data_plecarii == NULL;
-	}
-	this->data_plecarii = new char[strlen(a.data_plecarii) + 1];
 	strcpy_s(this->data_plecarii, strlen(a.data_plecarii) + 1, a.data_plecarii);
 
 	this->nr_loc_totale = a.nr_loc_totale;

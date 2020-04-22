0e0f57ae612dd98475a4cd8958d2a027136ebe5d
JacquelineK01 <61940347+JacquelineK01@users.noreply.github.com>
2020-04-22 12:23:29 +0300

    Update Service.h

--- Lab08/Lab08/Service.h
+++ Lab08/Lab08/Service.h
@@ -4,13 +4,19 @@ class Service
 {
 private:
 	FileRepository repo;
+	bool textOrCSV; // 0 txt , 1 CSV
 public:
-	Service(FileRepository repo) {};
-	Service() {};
+	Service(FileRepository repo) {
+		this->repo = repo;
+	};
+	Service() {
+		textOrCSV = 0;
+	};
 	~Service() {};
 	void changeTxtOrCSV(bool textOrCSV);
 	void rezervare(int cod, int nr_locuri);
 	std::vector<CalatorieAutobuz> calatoriiAutobuzInData(const char* data);
 	std::vector<CalatorieAvion> calatoriiAvionInData(const char* data);
+	void scrieCalatoriiInFisier(std::vector<CalatorieAutobuz> calatoriiAutobuz, std::vector<CalatorieAvion> calatoriiAvion);
 };
 

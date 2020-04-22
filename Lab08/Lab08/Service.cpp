5f38a5563c9ea6b8e8a251d0a65931fd8aadc54a
JacquelineK01 <61940347+JacquelineK01@users.noreply.github.com>
2020-04-22 12:22:44 +0300

    Update Service.cpp

--- Lab08/Lab08/Service.cpp
+++ Lab08/Lab08/Service.cpp
@@ -1,7 +1,8 @@
 #include "Service.h"
-
+#include <iostream>
 void Service::changeTxtOrCSV(bool textOrCSV)
 {
+	this->textOrCSV = textOrCSV;
 	repo.changeRepoType(textOrCSV);
 }
 
@@ -26,6 +27,14 @@ std::vector<CalatorieAvion> Service::calatoriiAvionInData(const char* data)
 	for (int i = 0; i < calatorii.size(); ++i)
 		if (strcmp(calatorii[i].getDataPlecarii(), data) == 0)
 			calatoriiFinale.push_back(calatorii[i]);
-
 	return calatoriiFinale;
+}
+
+void Service::scrieCalatoriiInFisier(std::vector<CalatorieAutobuz> calatoriiAutobuz, std::vector<CalatorieAvion> calatoriiAvion)
+{
+	std::cout << "BOOL: " << textOrCSV << "\n";
+	if (textOrCSV == 0)
+		repo.writeTripsToTxtFile(calatoriiAutobuz, calatoriiAvion);
+	else
+		repo.writeTripsToCSVFile(calatoriiAutobuz, calatoriiAvion);
 }
